ad7492303018da0cfa1893c9ceb1c219d6e018b7
Sotirios Paschalis <109233140+SotiriosPas@users.noreply.github.com>
2024-10-07 14:40:46 +0300

    added .c files

---
+++ a12f1.c
@@ -0,0 +1,21 @@
+/*
+*a12f1.c
+*/
+
+#include <stdio.h>
+#include "genlib.h"
+#include "simpio.h"
+
+main()
+{
+double ka8ara_tm,mikta_tm,DF,DT;
+printf("Dwse ta ka8ara tm tou spitiou? ");
+ka8ara_tm = GetReal();
+printf("Dwse ta mikta tm tou spitiou? ");
+mikta_tm = GetReal();
+DT = ka8ara_tm*1.33*61/365;
+DF = mikta_tm*0.13*61/365;
+printf("Ta DT einai : %g\n",DT);
+printf("O DF einai : %g\n",DF);
+system("pause");
+}
---
+++ a3f1.c
@@ -0,0 +1,23 @@
+/*
+*a3f1.c
+*/
+
+#include <stdio.h>
+#include "genlib.h"
+#include "simpio.h"
+
+main()
+{
+int syskeues,container1,container5,container20,container50;
+printf("Dwse to plhthos twn syskeuwn:");
+syskeues = GetInteger();
+container50 = syskeues / 50;
+container20 = (syskeues % 50) / 20;
+container5  = ((syskeues % 50) % 20) / 5;
+container1  = (((syskeues % 50) % 20) % 5)  / 1;
+printf("50 %d\n",container50);
+printf("20 %d\n",container20);
+printf("5 %d\n",container5);
+printf("1 %d\n",container1);
+system("pause");
+}
