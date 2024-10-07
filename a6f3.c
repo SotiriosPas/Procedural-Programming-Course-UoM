d4170bf48b7ac3fed28d0790649096f06b9b04b6
Sotirios Paschalis <109233140+SotiriosPas@users.noreply.github.com>
2024-10-07 14:50:28 +0300

    added .c files

---
+++ a6f3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "genlib.h"
+#include "simpio.h"
+
+main()
+{
+    int N,counter,a=0,b=1,c;
+    printf("Dwse ton arithmo twn orwn ths akolouthias Fibonnaci pou theleis na emfanistoun: ");
+    N=GetInteger();
+while (N<0)
+{
+    printf("Dwse thetiko arithmo: ");
+    N=GetInteger();
+}
+if (N==0)
+    system("pause");
+else
+{
+for (counter=0;counter<N;counter++)
+{
+if ( counter <= 1 )
+         c = counter;
+      else
+      {
+         c = a + b;
+         a = b;
+         b = c;
+      }
+      printf("%d\n",c);
+}
+system("pause");
+}
+}
